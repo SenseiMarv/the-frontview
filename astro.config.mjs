a9d6a83f662580c5008ba5dccfd0d6d2a48b5fdb
Marvin Stickel <marv.stickel@gmail.com>
2022-09-01 17:55:04 +0200

    Build: Add Astro integrations
    * Build: Add Astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,25 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
+import partytown from "@astrojs/partytown";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/serverless";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  integrations: [
+    react(),
+    sitemap(),
+    compress(),
+    partytown(),
+    tailwind(),
+    mdx(),
+    image(),
+  ],
+});

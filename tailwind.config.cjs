8e18590c91d59d97c5b037a7e1ac05602daadfff
Marvin Stickel <marv.stickel@gmail.com>
2022-09-07 15:36:07 +0200

    New: Add font families
    * New: Add font families
    * Build: Temporarily removed Astro file Linting while committing due to `no-unknown-property` issues. ESLint does not seem to know the Astro properties even if the corresponding plugin is set up

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        mono: ["Source Code Pro", ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 };

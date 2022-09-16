56ae73cf7e02099f14f8646856ca585063b74e88
Marvin Stickel <marv.stickel@gmail.com>
2022-09-16 14:29:08 +0200

    Update: Improve responsive design
    * Update: Improve responsive design
    * Update: Improve home page
    * Build: Add back in typing checks to build command

--- src/components/utils/sortPosts.ts
+++ src/components/utils/sortPosts.ts
@@ -2,10 +2,13 @@ import { compareDesc } from "date-fns";
 
 import type { Post } from "../../typings/Post";
 
-export const sortPostsByDate = (posts: Post[]) =>
-  posts.sort((postA, postB) =>
+export const sortPostsByDate = (posts: Post[]) => {
+  // `sort()` mutates the original array, so it should be called on a clone
+  const arr = posts;
+  return arr.sort((postA, postB) =>
     compareDesc(
       new Date(postA.frontmatter.date),
       new Date(postB.frontmatter.date)
     )
   );
+};

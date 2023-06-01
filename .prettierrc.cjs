d308fb7dd72ef93fffb485bd48f12723fe604c13
Marvin Stickel <marv.stickel@gmail.com>
2023-06-01 14:04:51 +0200

    Upgrade: Update all dependencies
    * Upgrade: Update all dependencies
    * Chore: Remove no longer needed config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -12,7 +12,6 @@ const config = {
       },
     },
   ],
-  astroAllowShorthand: false, // Disable the shorthand due to this bug: https://github.com/withastro/language-tools/issues/225
   tailwindConfig: "./tailwind.config.cjs",
 };
 
